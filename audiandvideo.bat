0f7b2bf32d65bd05ade1986742590c770154b058
bitelchux <bitelchux@yahoo.es>
2015-09-23 19:19:55 +0200

    df
    sdf

---
+++ audiandvideo.bat
@@ -0,0 +1,3 @@
+C:\Users\bitel_000\Documents\GitHub\bitelchux.github.io\ffmpeg -y -i C:\Users\bitel_000\Documents\GitHub\bitelchux.github.io\hansel-y-gretel.jpg -i C:\Users\bitel_000\Documents\GitHub\bitelchux.github.io\hansel-y-gretel.mp3 -map 0 -map 1 -codec copy  C:\Users\bitel_000\Documents\GitHub\bitelchux.github.io\hansel-y-gretel.avi
+ 
+pause
