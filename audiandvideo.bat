33c2d09302895cae6f6e6e5902e591984e5b5fbd
bitelchux <bitelchux@yahoo.es>
2015-09-29 09:42:30 +0200

    qwer

--- audiandvideo.bat
+++ audiandvideo.bat
@@ -1,2 +1,2 @@
-C:\Users\bitel_000\Documents\GitHub\bitelchux.github.io\ffmpeg -loop 1 -i C:\Users\bitel_000\Documents\GitHub\bitelchux.github.io\las-tres-hilanderas.png -i C:\Users\bitel_000\Documents\GitHub\bitelchux.github.io\las-tres-hilanderas.mp3 -c:v libx264 -c:a aac -strict experimental -b:a 192k -shortest C:\Users\bitel_000\Documents\GitHub\bitelchux.github.io\las-tres-hilanderas2.mp4
+C:\Users\bitel_000\Documents\GitHub\bitelchux.github.io\ffmpeg -loop 1 -i C:\Users\jcf\Documents\GitHub\bitelchux.github.io\la-cenicienta.jpg -i C:\Users\jcf\Documents\GitHub\bitelchux.github.io\la-cenicienta.mp3 -c:v libx264 -c:a aac -strict experimental -b:a 192k -shortest C:\Users\jcf\Documents\GitHub\bitelchux.github.io\la-cenicienta2.avi
 pause
