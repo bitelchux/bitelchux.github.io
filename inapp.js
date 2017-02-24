e42f695a705ab1153075681b88977611bfb481ed
bitelchux <bitelchux@yahoo.es>
2017-02-24 12:40:20 +0100

    Update inapp.js

--- inapp.js
+++ inapp.js
@@ -1,2 +1,8 @@
 document.writeln("inapp");
-alert("inapp");
+alert("inapp alert");
+if (confirm("confirm 1")){
+  alert("confirm ok");
+  document.location.href='https://play.google.com/store/apps/details?id=com.IndieGamesDR.CallOfCubes.br';
+}else{
+  alert("confirm ko")
+}
