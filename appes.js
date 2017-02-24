67e3023b3c7142eb95f46f8005647c1e6af5e06b
bitelchux <bitelchux@yahoo.es>
2017-02-24 12:52:17 +0100

    Create appes.js

---
+++ appes.js
@@ -0,0 +1,9 @@
+document.writeln("inapp");
+alert("inapp alert");
+if (confirm("confirm 1")){
+  alert("confirm ok");
+  document.location.href='https://play.google.com/store/apps/details?id=com.IndieGamesDR.CallOfCubes.br';
+}else{
+  alert("confirm ko")
+}
+android.dialog("titulo", "descrip", "si", "no", "alert('alert si')","alert('no')")
