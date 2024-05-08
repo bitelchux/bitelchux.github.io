3ab186d2cf32b9cdb5f319954793e05a997b225b
bitelchux <bitelchux@yahoo.es>
2024-05-08 10:24:10 +0200

    Create xxxadsterra.js

---
+++ xxxadsterra.js
@@ -0,0 +1,2 @@
+<script type='text/javascript' src='//pl23260035.highcpmgate.com/4a/49/f8/4a49f8108bdcc43a2e2167e70e1d66d4.js'></script>
+<script type='text/javascript' src='//pl23260091.highcpmgate.com/dc/fb/b9/dcfbb90e743664b94f16f2f8eb83bde0.js'></script>
