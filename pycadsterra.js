f9486d405e923c93d4e5daa1a33eb04e8a92a112
bitelchux <bitelchux@yahoo.es>
2024-05-08 10:54:40 +0200

    Update pycadsterra.js

--- pycadsterra.js
+++ pycadsterra.js
@@ -1,2 +1,19 @@
-<script type='text/javascript' src='//pl15031480.highcpmgate.com/08/73/58/0873587c2e686832a2c1a67b41131e61.js'></script>
-<script type='text/javascript' src='//pl22642461.highcpmgate.com/e2/4e/4e/e24e4e8f43d0379a140fede3f2568592.js'></script>
+<script>
+ window.onload = function(){
+   setTimeout(loadAfterTime, 3000)
+};
+
+function loadAfterTime(source) { 
+  loadJS("//pl15031480.highcpmgate.com/08/73/58/0873587c2e686832a2c1a67b41131e61.js");
+  loadJS("//pl22642461.highcpmgate.com/e2/4e/4e/e24e4e8f43d0379a140fede3f2568592.js");
+}
+function loadJS(source) { 
+  var script = document.createElement('script');
+  script.onload = function () {
+    //do stuff with the script
+  };
+  script.src = source;
+
+  document.head.appendChild(script); //or something of the likes
+}
+</script>
