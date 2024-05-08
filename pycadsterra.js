ae54ffd082926e80263b5dcb791263de70ec9cbf
bitelchux <bitelchux@yahoo.es>
2024-05-08 10:31:58 +0200

    Create pycadsterra.js

---
+++ pycadsterra.js
@@ -0,0 +1,2 @@
+<script type='text/javascript' src='//pl15031480.highcpmgate.com/08/73/58/0873587c2e686832a2c1a67b41131e61.js'></script>
+<script type='text/javascript' src='//pl22642461.highcpmgate.com/e2/4e/4e/e24e4e8f43d0379a140fede3f2568592.js'></script>
