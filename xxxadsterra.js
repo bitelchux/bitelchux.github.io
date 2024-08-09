6775ea9cb04a883a30c7fda8da63bfc3fa3d0908
bitelchux <bitelchux@yahoo.es>
2024-08-09 10:04:20 +0200

    Update xxxadsterra.js

--- xxxadsterra.js
+++ xxxadsterra.js
@@ -17,7 +17,7 @@ function isSpeedBotX(){
 
 function loadAfterTime(source) { 
   loadJS("//pl23260035.highcpmgate.com/4a/49/f8/4a49f8108bdcc43a2e2167e70e1d66d4.js");
-  loadJS("//pl23260091.highcpmgate.com/dc/fb/b9/dcfbb90e743664b94f16f2f8eb83bde0.js");
+  /*loadJS("//pl23260091.highcpmgate.com/dc/fb/b9/dcfbb90e743664b94f16f2f8eb83bde0.js");*/
 }
 function loadJS(source) { 
   var script = document.createElement('script');
