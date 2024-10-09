9de3d89b15f64f9cf67274a03d74f1d986890e8b
bitelchux <bitelchux@yahoo.es>
2024-10-09 09:07:13 +0200

    Update adsinfobolas.js

--- adsinfobolas.js
+++ adsinfobolas.js
@@ -27,7 +27,7 @@ function loadAfterTime(source) {
 	loadJS("//pl18385895.profitablegatecpm.com/e8/e9/23/e8e9237d7e6c9674010946d09842f465.js");
 	loadJS("//pl18385961.profitablegatecpm.com/a4/e7/55/a4e7557f2067c4c0f922d9747a61a17f.js");
 	loadJS("//js.juicyads.com/jp.php?c=34e4y213q274u4q2x294x26444&u=http%3A%2F%2Fwww.juicyads.rocks");
-
+/*
 	(function(w,d,o,g,r,a,m){
 	var cid=(Math.random()*1e17).toString(36);d.write('<div id="'+cid+'" ></div>');
 	w[r]=w[r]||function(){(w[r+'l']=w[r+'l']||[]).push(arguments)};
@@ -47,7 +47,7 @@ function loadAfterTime(source) {
 	p.parentNode.insertBefore(a,p)}}e(g,w,r);
 	w[r](cid,{id:981791016,domain:w[r+'h']});
 	})(window,document,'script',['adsbetnet.com'],'ABNS');
-	
+*/	
 }
 function loadJS2(source,param) { 
 	var script = document.createElement('script');
