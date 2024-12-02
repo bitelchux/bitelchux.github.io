27d457110cb05bfa560a8878d3b71d0db17fad21
bitelchux <bitelchux@yahoo.es>
2024-12-02 14:13:04 +0100

    Update adsinfobolas.js

--- adsinfobolas.js
+++ adsinfobolas.js
@@ -16,32 +16,21 @@ function isSpeedBotX(){
 };
 var juicy_adzone = '1037514';
 function loadAfterTime(source) { 
-
+/*
 	(function(){var u=window,r="d30a423e37fe9b7888b7e8f56a20e472",z=[["siteId",861*328+861-791*531+5072267],["minBid",0.0001],["popundersPerIP","0"],["delayBetween",0],["default","https://300incestos.com/linkrotator.php"],["defaultPerDay",0],["topmostLayer","auto"]],e=["d3d3LmludGVsbGlnZW5jZWFkeC5jb20vd1dzL3NFYy9ybDIwbi5taW4uanM=","ZDJrbHg4N2Jnem5nY2UuY2xvdWRmcm9udC5uZXQvZnJvdWdoLmpz"],f=-1,t,k,c=function(){clearTimeout(k);f++;if(e[f]&&!(1755771556000<(new Date).getTime()&&1<f)){t=u.document.createElement("script");t.type="text/javascript";t.async=!0;var y=u.document.getElementsByTagName("script")[0];t.src="https://"+atob(e[f]);t.crossOrigin="anonymous";t.onerror=c;t.onload=function(){clearTimeout(k);u[r.slice(0,16)+r.slice(0,16)]||c()};k=setTimeout(c,5E3);y.parentNode.insertBefore(t,y)}};if(!u[r]){try{Object.freeze(u[r]=z)}catch(e){}c()}})();
-
+*/
 /*
 	loadJS2("//ss.mrmnd.com/dynamic.js","4923d013-8239-4362-985e-a017c5023cb8");
 	loadJS2("//ss.mrmnd.com/dynamic.js","a5d11fd3-d6d7-4261-8b24-636bc8f420e8");
 	loadJS2("//ss.mrmnd.com/dynamic.js","ea51f9fc-365f-4a9d-ba21-5f2635ae5179");
 	loadJS("https://ss.mrmnd.com/static/6f6e6f72-a43e-4731-93e9-df1df5076ecd.js");
 	*/
-	
+	/*
 	loadJS("//pl18385895.profitablegatecpm.com/e8/e9/23/e8e9237d7e6c9674010946d09842f465.js");
 	loadJS("//pl18385961.profitablegatecpm.com/a4/e7/55/a4e7557f2067c4c0f922d9747a61a17f.js");
 	loadJS("//js.juicyads.com/jp.php?c=34e4y213q274u4q2x294x26444&u=http%3A%2F%2Fwww.juicyads.rocks");
 	loadJS("//poweredby.jads.co/js/jfc.js");
 	loadJS("//www.freeprivacypolicy.com/public/cookie-consent/4.1.0/cookie-consent.js");
-/*hilltopads push
-(function(ephwwo){
-var d = document,
-    s = d.createElement('script'),
-    l = d.scripts[d.scripts.length - 1];
-s.settings = ephwwo || {};
-s.src = "\/\/private-hide.com\/bYX\/V.sld\/GGlo0DYYWQcg\/xeCm\/9su\/Z\/UmlykBP\/TWUy3\/MKTqEU0WMfjVA\/tJNnTycuxGMLTcQ-yjMwQg";
-s.async = true;
-s.referrerPolicy = 'no-referrer-when-downgrade';
-l.parentNode.insertBefore(s, l);
-})({})
 */
 	 document.addEventListener('DOMContentLoaded', function() {
 		cookieconsent.run({
