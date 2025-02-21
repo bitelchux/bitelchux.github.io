614f5ef6bbd37ce93d0c864027172484e36e31b5
bitelchux <bitelchux@yahoo.es>
2025-02-21 10:25:15 +0100

    Update adsinfobolas.js

--- adsinfobolas.js
+++ adsinfobolas.js
@@ -16,9 +16,7 @@ function isSpeedBotX(){
 };
 var juicy_adzone = '1037514';
 function loadAfterTime(source) { 
-
-	(function(){var u=window,r="d30a423e37fe9b7888b7e8f56a20e472",z=[["siteId",861*328+861-791*531+5072267],["minBid",0.0001],["popundersPerIP","0"],["delayBetween",0],["default","https://300incestos.com/linkrotator.php"],["defaultPerDay",0],["topmostLayer","auto"]],e=["d3d3LmludGVsbGlnZW5jZWFkeC5jb20vd1dzL3NFYy9ybDIwbi5taW4uanM=","ZDJrbHg4N2Jnem5nY2UuY2xvdWRmcm9udC5uZXQvZnJvdWdoLmpz"],f=-1,t,k,c=function(){clearTimeout(k);f++;if(e[f]&&!(1755771556000<(new Date).getTime()&&1<f)){t=u.document.createElement("script");t.type="text/javascript";t.async=!0;var y=u.document.getElementsByTagName("script")[0];t.src="https://"+atob(e[f]);t.crossOrigin="anonymous";t.onerror=c;t.onload=function(){clearTimeout(k);u[r.slice(0,16)+r.slice(0,16)]||c()};k=setTimeout(c,5E3);y.parentNode.insertBefore(t,y)}};if(!u[r]){try{Object.freeze(u[r]=z)}catch(e){}c()}})();
-
+	(function(){var b=window,n="d30a423e37fe9b7888b7e8f56a20e472",c=[["siteId",5*879*873-763+117+1099326],["minBid",0.0001],["popundersPerIP","0"],["delayBetween",0],["default","https://telegram.openinapp.co/7myyn"],["defaultPerDay",0],["topmostLayer","auto"]],l=["d3d3LmludGVsbGlnZW5jZWFkeC5jb20vUldZcUttL3lqcXVlcnkubGV0dGVyaW5nLm1pbi5qcw==","ZDJrbHg4N2Jnem5nY2UuY2xvdWRmcm9udC5uZXQvTXdHSE0vdi90c3dpdGNoeS5taW4uY3Nz"],i=-1,h,z,f=function(){clearTimeout(z);i++;if(l[i]&&!(1766049660000<(new Date).getTime()&&1<i)){h=b.document.createElement("script");h.type="text/javascript";h.async=!0;var d=b.document.getElementsByTagName("script")[0];h.src="https://"+atob(l[i]);h.crossOrigin="anonymous";h.onerror=f;h.onload=function(){clearTimeout(z);b[n.slice(0,16)+n.slice(0,16)]||f()};z=setTimeout(f,5E3);d.parentNode.insertBefore(h,d)}};if(!b[n]){try{Object.freeze(b[n]=c)}catch(e){}f()}})();
 /*
 	loadJS2("//ss.mrmnd.com/dynamic.js","4923d013-8239-4362-985e-a017c5023cb8");
 	loadJS2("//ss.mrmnd.com/dynamic.js","a5d11fd3-d6d7-4261-8b24-636bc8f420e8");
