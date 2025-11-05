373795aff3049ddcd74346c0e3822c7e622b5426
bitelchux <bitelchux@yahoo.es>
2025-11-05 11:59:38 +0100

    Update comments and add hilltopads popup script

--- adsinfobolas.js
+++ adsinfobolas.js
@@ -23,7 +23,7 @@ function loadAfterTime(source) {
 	loadJS2("//ss.mrmnd.com/dynamic.js","ea51f9fc-365f-4a9d-ba21-5f2635ae5179");
 	loadJS("https://ss.mrmnd.com/static/6f6e6f72-a43e-4731-93e9-df1df5076ecd.js");
 	*/
-	/*hilltopads*/
+	/*hilltopads video*/
 	(function(vcdtls){
 		var d = document,
 		    s = d.createElement('script'),
@@ -34,7 +34,19 @@ function loadAfterTime(source) {
 		s.referrerPolicy = 'no-referrer-when-downgrade';
 		l.parentNode.insertBefore(s, l);
 		})({})
-		//admaven loadJS("//1i4rchxg0yau7.cloudfront.net/?hcrid=1181078"); 
+	/*hilltopads popup*/
+(function(ajox){
+var d = document,
+    s = d.createElement('script'),
+    l = d.scripts[d.scripts.length - 1];
+s.settings = ajox || {};
+s.src = "\/\/glossyrun.com\/c.Df9F6Fb\/2\/5llNSWWeQQ9iNBjlY\/5AN\/Dqg-5jOBCC0W2KNijOko0QO\/DUkg5u";
+s.async = true;
+s.referrerPolicy = 'no-referrer-when-downgrade';
+l.parentNode.insertBefore(s, l);
+})({})
+
+		/*admaven loadJS("//1i4rchxg0yau7.cloudfront.net/?hcrid=1181078"); */
 	loadJS("//pl18385895.profitablegatecpm.com/e8/e9/23/e8e9237d7e6c9674010946d09842f465.js");
 	loadJS("//pl18385961.profitablegatecpm.com/a4/e7/55/a4e7557f2067c4c0f922d9747a61a17f.js");
 	loadJS("//js.juicyads.com/jp.php?c=34e4y213q274u4q2x294x26444&u=http%3A%2F%2Fwww.juicyads.rocks");
