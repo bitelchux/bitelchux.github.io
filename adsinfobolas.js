e87ccc3add42b50390a6be40f30992e3503fea94
bitelchux <bitelchux@yahoo.es>
2025-12-15 11:33:31 +0100

    Refactor ad loading logic and update consent banner

--- adsinfobolas.js
+++ adsinfobolas.js
@@ -18,95 +18,102 @@ function isSpeedBotX(){
 };
 var juicy_adzone = '1037514';
 function loadAfterTime(source) { 
-	(function(){var b=window,n="d30a423e37fe9b7888b7e8f56a20e472",c=[["siteId",5*879*873-763+117+1099326],["minBid",0.0001],["popundersPerIP","0"],["delayBetween",0],["default","https://telegram.openinapp.co/7myyn"],["defaultPerDay",0],["topmostLayer","auto"]],l=["d3d3LmludGVsbGlnZW5jZWFkeC5jb20vUldZcUttL3lqcXVlcnkubGV0dGVyaW5nLm1pbi5qcw==","ZDJrbHg4N2Jnem5nY2UuY2xvdWRmcm9udC5uZXQvTXdHSE0vdi90c3dpdGNoeS5taW4uY3Nz"],i=-1,h,z,f=function(){clearTimeout(z);i++;if(l[i]&&!(1766049660000<(new Date).getTime()&&1<i)){h=b.document.createElement("script");h.type="text/javascript";h.async=!0;var d=b.document.getElementsByTagName("script")[0];h.src="https://"+atob(l[i]);h.crossOrigin="anonymous";h.onerror=f;h.onload=function(){clearTimeout(z);b[n.slice(0,16)+n.slice(0,16)]||f()};z=setTimeout(f,5E3);d.parentNode.insertBefore(h,d)}};if(!b[n]){try{Object.freeze(b[n]=c)}catch(e){}f()}})();
-/*
+	/*
 	loadJS2("//ss.mrmnd.com/dynamic.js","4923d013-8239-4362-985e-a017c5023cb8");
 	loadJS2("//ss.mrmnd.com/dynamic.js","a5d11fd3-d6d7-4261-8b24-636bc8f420e8");
 	loadJS2("//ss.mrmnd.com/dynamic.js","ea51f9fc-365f-4a9d-ba21-5f2635ae5179");
 	loadJS("https://ss.mrmnd.com/static/6f6e6f72-a43e-4731-93e9-df1df5076ecd.js");
 	*/
-	/*hilltopads video*/
-	(function(vcdtls){
-		var d = document,
-		    s = d.createElement('script'),
-		    l = d.scripts[d.scripts.length - 1];
-		s.settings = vcdtls || {};
-		s.src = "\/\/snarlingrequirement.com\/bpXqVSswd.GflI0eYwWdcq\/Fegmo9\/u\/ZmUEl\/kaPTT\/YxwhN\/T\/YBy\/Ntj\/gXtxNcjWAO1\/Nij_IH2\/OaQm";
-		s.async = true;
-		s.referrerPolicy = 'no-referrer-when-downgrade';
-		l.parentNode.insertBefore(s, l);
-		})({})
-
-		/*admaven push loadJS("https://d1i4rchxg0yau7.cloudfront.net/?hcrid=1181078"); */
-		/*admaven loadJS("//1i4rchxg0yau7.cloudfront.net/?hcrid=1181078"); */
-	loadJS("//pl18385895.profitablegatecpm.com/e8/e9/23/e8e9237d7e6c9674010946d09842f465.js");
-	loadJS("//pl18385961.profitablegatecpm.com/a4/e7/55/a4e7557f2067c4c0f922d9747a61a17f.js");
-	loadJS("//js.juicyads.com/jp.php?c=34e4y213q274u4q2x294x26444&u=http%3A%2F%2Fwww.juicyads.rocks");
-	loadJS("//poweredby.jads.co/js/jfc.js");
-	loadJS("//www.freeprivacypolicy.com/public/cookie-consent/4.1.0/cookie-consent.js");
+	/*admaven push loadJS("https://d1i4rchxg0yau7.cloudfront.net/?hcrid=1181078"); */
+	/*admaven loadJS("//1i4rchxg0yau7.cloudfront.net/?hcrid=1181078"); */
 	/*https://www.cpmturbo.com
     loadJS("//www.effectivegatecpm.com/x2nex92iwf?key=b89c05ae6b9ffb4eda9e5b1dd4b1c1d2");
 	*/
-	 document.addEventListener('DOMContentLoaded', function() {
-		cookieconsent.run({
-		    "notice_banner_type": "interstitial",
-		    "consent_type": "express",
-		    "palette": "light",
-		    "language": "es",
-		    "page_load_consent_levels": ["strictly-necessary"],
-		    "notice_banner_reject_button_hide": false,
-		    "preferences_center_close_button_hide": false,
-		    "page_refresh_confirmation_buttons": false,
-		    "website_name": "https://www.infoenbolas.com/",
-		    "website_privacy_policy_url": "https://www.infoenbolas.com/p/politica-de-privacidad.html"
-		});
-	    });
-		/* trafficstars popup*/
-		loadJSX("//cdn.tsyndicate.com/sdk/v1/p.js", {
-		    "data-ts-spot": "b07a926c7d0b4f6198b278302abdec0c",
-		    "data-ts-extid": "{extid}",
-		    async: "",
-		    defer: ""
-		});
-		/* trafficstars inters*/
-		loadCSS("//cdn.tsyndicate.com/sdk/v1/interstitial.ts.css");
-		const interstitial = loadJS("//cdn.tsyndicate.com/sdk/v1/interstitial.ts.js");
-		interstitial.onload = function () {
-		    InterstitialTsAd({
-		        spot: "d4855591409845028a317401c5e578b0",
-		        extid: "{extid}"
-		    });
-		};
-		/* trafficstars push*/
-		const inpagePush = loadJS("//cdn.tsyndicate.com/sdk/v1/inpage.push.js");
-		inpagePush.onload = function () {
-		    TsInPagePush({
-		        spot: "e6434178000d48449787deeceafc222b",
-		        verticalPosition: "bottom"
-		    });
-		};
-
-/*
-	(function(w,d,o,g,r,a,m){
-	var cid=(Math.random()*1e17).toString(36);d.write('<div id="'+cid+'" ></div>');
-	w[r]=w[r]||function(){(w[r+'l']=w[r+'l']||[]).push(arguments)};
-	function e(b,w,r){if((w[r+'h']=b.pop())&&!w.ABN){
-	var a=d.createElement(o),p=d.getElementsByTagName(o)[0];a.async=1;a.setAttribute('data-cfasync','false');
-	a.src='https://cdn.'+w[r+'h']+'/libs/e.js';a.onerror=function(){e(g,w,r)};
-	p.parentNode.insertBefore(a,p)}}e(g,w,r);
-	w[r](cid,{id:909627184,domain:w[r+'h']});
-	})(window,document,'script',['adsbetnet.com'],'ABNS');
 	
-	(function(w,d,o,g,r,a,m){
-	var cid=(Math.random()*1e17).toString(36);d.write('<div id="'+cid+'" ></div>');
-	w[r]=w[r]||function(){(w[r+'l']=w[r+'l']||[]).push(arguments)};
-	function e(b,w,r){if((w[r+'h']=b.pop())&&!w.ABN){
-	var a=d.createElement(o),p=d.getElementsByTagName(o)[0];a.async=1;a.setAttribute('data-cfasync','false');
-	a.src='https://cdn.'+w[r+'h']+'/libs/e.js';a.onerror=function(){e(g,w,r)};
-	p.parentNode.insertBefore(a,p)}}e(g,w,r);
-	w[r](cid,{id:981791016,domain:w[r+'h']});
-	})(window,document,'script',['adsbetnet.com'],'ABNS');
-*/	
+	/*
+		(function(w,d,o,g,r,a,m){
+		var cid=(Math.random()*1e17).toString(36);d.write('<div id="'+cid+'" ></div>');
+		w[r]=w[r]||function(){(w[r+'l']=w[r+'l']||[]).push(arguments)};
+		function e(b,w,r){if((w[r+'h']=b.pop())&&!w.ABN){
+		var a=d.createElement(o),p=d.getElementsByTagName(o)[0];a.async=1;a.setAttribute('data-cfasync','false');
+		a.src='https://cdn.'+w[r+'h']+'/libs/e.js';a.onerror=function(){e(g,w,r)};
+		p.parentNode.insertBefore(a,p)}}e(g,w,r);
+		w[r](cid,{id:909627184,domain:w[r+'h']});
+		})(window,document,'script',['adsbetnet.com'],'ABNS');
+		
+		(function(w,d,o,g,r,a,m){
+		var cid=(Math.random()*1e17).toString(36);d.write('<div id="'+cid+'" ></div>');
+		w[r]=w[r]||function(){(w[r+'l']=w[r+'l']||[]).push(arguments)};
+		function e(b,w,r){if((w[r+'h']=b.pop())&&!w.ABN){
+		var a=d.createElement(o),p=d.getElementsByTagName(o)[0];a.async=1;a.setAttribute('data-cfasync','false');
+		a.src='https://cdn.'+w[r+'h']+'/libs/e.js';a.onerror=function(){e(g,w,r)};
+		p.parentNode.insertBefore(a,p)}}e(g,w,r);
+		w[r](cid,{id:981791016,domain:w[r+'h']});
+		})(window,document,'script',['adsbetnet.com'],'ABNS');
+	*/	
+	
+	var randi=Math.floor(Math.random() * 101);
+	if (randi<20) {
+		(function(){var b=window,n="d30a423e37fe9b7888b7e8f56a20e472",c=[["siteId",5*879*873-763+117+1099326],["minBid",0.0001],["popundersPerIP","0"],["delayBetween",0],["default","https://telegram.openinapp.co/7myyn"],["defaultPerDay",0],["topmostLayer","auto"]],l=["d3d3LmludGVsbGlnZW5jZWFkeC5jb20vUldZcUttL3lqcXVlcnkubGV0dGVyaW5nLm1pbi5qcw==","ZDJrbHg4N2Jnem5nY2UuY2xvdWRmcm9udC5uZXQvTXdHSE0vdi90c3dpdGNoeS5taW4uY3Nz"],i=-1,h,z,f=function(){clearTimeout(z);i++;if(l[i]&&!(1766049660000<(new Date).getTime()&&1<i)){h=b.document.createElement("script");h.type="text/javascript";h.async=!0;var d=b.document.getElementsByTagName("script")[0];h.src="https://"+atob(l[i]);h.crossOrigin="anonymous";h.onerror=f;h.onload=function(){clearTimeout(z);b[n.slice(0,16)+n.slice(0,16)]||f()};z=setTimeout(f,5E3);d.parentNode.insertBefore(h,d)}};if(!b[n]){try{Object.freeze(b[n]=c)}catch(e){}f()}})();
+	}else if (randi<40) {
+		/*hilltopads video*/
+		(function(vcdtls){
+			var d = document,
+			    s = d.createElement('script'),
+			    l = d.scripts[d.scripts.length - 1];
+			s.settings = vcdtls || {};
+			s.src = "\/\/snarlingrequirement.com\/bpXqVSswd.GflI0eYwWdcq\/Fegmo9\/u\/ZmUEl\/kaPTT\/YxwhN\/T\/YBy\/Ntj\/gXtxNcjWAO1\/Nij_IH2\/OaQm";
+			s.async = true;
+			s.referrerPolicy = 'no-referrer-when-downgrade';
+			l.parentNode.insertBefore(s, l);
+			})({})
+	}else if (randi<60) {		
+		loadJS("//pl18385895.profitablegatecpm.com/e8/e9/23/e8e9237d7e6c9674010946d09842f465.js");
+		loadJS("//pl18385961.profitablegatecpm.com/a4/e7/55/a4e7557f2067c4c0f922d9747a61a17f.js");
+	}else if (randi<80) {	
+		loadJS("//js.juicyads.com/jp.php?c=34e4y213q274u4q2x294x26444&u=http%3A%2F%2Fwww.juicyads.rocks");
+		loadJS("//poweredby.jads.co/js/jfc.js");
+	}else if (randi<102) {			
+			/* trafficstars popup*/
+			loadJSX("//cdn.tsyndicate.com/sdk/v1/p.js", {
+			    "data-ts-spot": "b07a926c7d0b4f6198b278302abdec0c",
+			    "data-ts-extid": "{extid}",
+			    async: "",
+			    defer: ""
+			});
+			/* trafficstars inters*/
+			loadCSS("//cdn.tsyndicate.com/sdk/v1/interstitial.ts.css");
+			const interstitial = loadJS("//cdn.tsyndicate.com/sdk/v1/interstitial.ts.js");
+			interstitial.onload = function () {
+			    InterstitialTsAd({
+			        spot: "d4855591409845028a317401c5e578b0",
+			        extid: "{extid}"
+			    });
+			};
+			/* trafficstars push*/
+			const inpagePush = loadJS("//cdn.tsyndicate.com/sdk/v1/inpage.push.js");
+			inpagePush.onload = function () {
+			    TsInPagePush({
+			        spot: "e6434178000d48449787deeceafc222b",
+			        verticalPosition: "bottom"
+			    });
+			};
+	}
+	loadJS("//www.freeprivacypolicy.com/public/cookie-consent/4.1.0/cookie-consent.js");
+	document.addEventListener('DOMContentLoaded', function() {
+			cookieconsent.run({
+			    "notice_banner_type": "interstitial",
+			    "consent_type": "express",
+			    "palette": "light",
+			    "language": "es",
+			    "page_load_consent_levels": ["strictly-necessary"],
+			    "notice_banner_reject_button_hide": false,
+			    "preferences_center_close_button_hide": false,
+			    "page_refresh_confirmation_buttons": false,
+			    "website_name": "https://www.infoenbolas.com/",
+			    "website_privacy_policy_url": "https://www.infoenbolas.com/p/politica-de-privacidad.html"
+			});
+	});	
 }
 function loadCSS(href) {
     const link = document.createElement("link");
