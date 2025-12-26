494a31f055e1174b8942d63b6bcb67fb91992574
bitelchux <bitelchux@yahoo.es>
2025-12-26 20:47:41 +0100

    Comment out referrer check in microblogbeebom.js

--- microblogbeebom.js
+++ microblogbeebom.js
@@ -1,8 +1,9 @@
 (function () {
-  /* from lgtb.com */
+  /* from lgtb.com 
   if (document.referrer && document.referrer.includes('donlgbt.com')) {
       window.location.href = 'https://promptchan.com/m/tJEjzfPGqgXGCw2EVpiQf1YQ60q1/donlgt?landing=/gay-ai-porn';
   }
+  */
   /* aviso legal*/
   const lang = (navigator.language || navigator.userLanguage || 'en').toLowerCase();
  const article = document.querySelector('article');
