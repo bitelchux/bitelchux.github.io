ae8d6b881d3927f374a218fddd4514f79b73e0c6
bitelchux <bitelchux@yahoo.es>
2026-03-13 10:00:13 +0100

    Refactor adsterras initialization and condition check

--- microblogbeebom.js
+++ microblogbeebom.js
@@ -484,17 +484,18 @@ function loadAfterTime() {
 	  document.querySelectorAll('div.menu-toggle').forEach(d => d.style.display='none');
       return;
   }
+	var adsterras=[];
+	adsterras["acelstore.es"]="https://compiledonatevanity.com/73/47/b2/7347b200da990018c3b935e57198ab71.js";
+	adsterras["cancionespronunciacion.com"]="https://compiledonatevanity.com/8a/10/61/8a10618c1230962eea625d64a3f906bb.js";
+	adsterras["cuentocorto.es"]="https://compiledonatevanity.com/5b/b2/36/5bb23664c43ce429fa39338771740942.js";
+	adsterras["especiespro.es"]="https://compiledonatevanity.com/d9/a0/44/d9a044ac868473bc3ec4b1d0f4b099b4.js";
+	adsterras["fanfic.es"]="https://compiledonatevanity.com/87/c9/83/87c98332e93283556ccaa50baa0b4993.js";
+	adsterras["calculatunota.es"]="https://compiledonatevanity.com/dc/60/79/dc60790f983d67a5fe224259442f0a21.js";
+	adsterras["multicomix.com"]="https://compiledonatevanity.com/03/f2/bb/03f2bb6fa82fe45c75dbc9e00dc4978e.js|https://compiledonatevanity.com/8a/b5/7b/8ab57b1fe35feb6cf0322988b231bac2.js";
+	
 	/*wsta variable la usa google adsense*/
-	if (window.google_srt==undefined && window.conotrosads==undefined ){
+	if (adsterras[window.location.host]!=undefined || (window.google_srt==undefined && window.conotrosads==undefined) ){
 		/*adsterra pyc social bar*/
-		var adsterras=[];
-		adsterras["acelstore.es"]="https://compiledonatevanity.com/73/47/b2/7347b200da990018c3b935e57198ab71.js";
-		adsterras["cancionespronunciacion.com"]="https://compiledonatevanity.com/8a/10/61/8a10618c1230962eea625d64a3f906bb.js";
-		adsterras["cuentocorto.es"]="https://compiledonatevanity.com/5b/b2/36/5bb23664c43ce429fa39338771740942.js";
-		adsterras["especiespro.es"]="https://compiledonatevanity.com/d9/a0/44/d9a044ac868473bc3ec4b1d0f4b099b4.js";
-		adsterras["fanfic.es"]="https://compiledonatevanity.com/87/c9/83/87c98332e93283556ccaa50baa0b4993.js";
-		adsterras["calculatunota.es"]="https://compiledonatevanity.com/dc/60/79/dc60790f983d67a5fe224259442f0a21.js";
-		adsterras["multicomix.com"]="https://compiledonatevanity.com/03/f2/bb/03f2bb6fa82fe45c75dbc9e00dc4978e.js|https://compiledonatevanity.com/8a/b5/7b/8ab57b1fe35feb6cf0322988b231bac2.js";
 		if (adsterras[window.location.host]!=undefined){
 			var scriptsadsterras = adsterras[window.location.host].split("|");
     
