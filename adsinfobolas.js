99981736459b03392fdbedf5d564df611949613f
bitelchux <bitelchux@yahoo.es>
2026-05-18 17:28:30 +0200

    Fix syntax error in adsinfobolas.js

--- adsinfobolas.js
+++ adsinfobolas.js
@@ -267,7 +267,7 @@ window.addEventListener("load", () => {
                             loadRandomAds(); 
                         }
                     });
-            }
+            });
         });
         
     }, 1000);
